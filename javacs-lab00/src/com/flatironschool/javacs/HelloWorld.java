13de328829b155723aa995416ab70c3ec65c05c3
jmgamer12 <jgsmartguy23@gmail.com>
2016-03-23 15:38:00 -0500

    Refined and working code

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -1,14 +1,13 @@
 package com.flatironschool.javacs;
 
 public class HelloWorld {
-    public static Double getVersion(Double versionNum) {
-		versionNum = Double.parseDouble(System.getProperty("java.version"));
-		return versionNum;
+    public static Double getVersion() {
+		    return Double.parseDouble(System.getProperty("java.specification.version"));
         // I won't work!
     }
 
     public static void main(String[] args) {
-		System.out.println(getVersion());
+		    System.out.println(getVersion());
 	    // you can test the output of getVersion() here
     }
 }
